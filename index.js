39d54e447b7682f681605587f82cb1cbc4bb451a
Thiago Cruz Machado <thiago.machado2498@gmail.com>
2023-10-17 22:16:43 -0300

    v1

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+
+//Database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conectado ao banco de dados')
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+//Estou dizendo para o Express usar o EJS como view engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//rotas
+app.get("/", (req, res) => {
+    Pergunta.findAll({
+        raw: true, order: [
+            //ASC = Crescente || DESC = Decrescente 
+            ['id', 'DESC'] // ordem do maior id pra menor 
+        ]
+    }).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    //res.send("Formulário recebido! titulo: " + titulo + "/" + "descricao: " + descricao);
+    //Pergunta.create() é basicamente um código INSERT INTO....
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: { id: id }
+    }).then(pergunta => {
+
+        if (pergunta != undefined) {//Pergunta encontrada
+
+            Resposta.findAll({
+                where: { perguntaId: pergunta.id },
+                order: [
+                    ['id', 'DESC']
+                ]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+        } else {//Não encontrada
+            res.redirect("/")
+        }
+    })
+});
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
+    })
+});
+
+app.listen(8080, () => {
+    console.log("App rodando!!");
+});
